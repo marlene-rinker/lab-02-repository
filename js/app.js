f21e35efb8ba87a12724b4fc22ad11111b0ced41
marlene-rinker <marlene.rinker@prtechnologies.com>
2020-05-06 20:45:52 -0700

    progress with Chance

--- js/app.js
+++ js/app.js
@@ -1,6 +1,7 @@
+/* global Handlebars */
 'use strict';
 
-AnimalImage.filterArray = [];
+AnimalImage.keywords = [];
 AnimalImage.allImages = [];
 
 function AnimalImage(image_url, title, description, keyword, horns) {
@@ -10,55 +11,26 @@ function AnimalImage(image_url, title, description, keyword, horns) {
   this.keyword = keyword;
   this.horns = horns;
 }
+TODO:
+// In the front end, the section with id of photo-template needs to have a class= to handlebars version of keyword
+// Console logs in initial render to page function - image array and keyword array - make sure we're getting it back 
+// probably getting it back and not getting it right on the front end
 
-// AnimalImage.prototype.renderDropdown = function () {
-//   if (!AnimalImage.filterArray.includes(this.keyword)) {
-//     const $animalFilterClone = $('option:first-child').clone();
-//     $animalFilterClone.attr('value', this.keyword);
-//     $animalFilterClone.text(this.keyword);
-//     $('select').append($animalFilterClone);
-//     AnimalImage.filterArray.push(this.keyword);
-//   }
-// };
-
-AnimalImage.prototype.renderImages = function (filePath) {
-const $animalTemplate = $('#photoTemplate').html();
-const result = Handlebars.compile($animalTemplate);
-return result(this);
-// $('main').append(result);
-
-  // const $animalTemplateClone = $('#photo-template').clone();
-  // $animalTemplateClone.removeAttr('id').addClass('photo');
-  // $animalTemplateClone.removeAttr('id').addClass(filePath.slice(5, 11));
-  // $animalTemplateClone.find('h2').text(this.title);
-  // $animalTemplateClone.find('img').attr('src', this.image_url);
-  // $animalTemplateClone.find('p').text(this.description);
-  // // add data containing keyword to $animalTemplateClone
-  // $animalTemplateClone.data('keyword', this.keyword);
-  // $('main').append($animalTemplateClone);
+AnimalImage.prototype.renderImages = function () {
+  const $animalTemplate = $('#photoTemplate').html();
+  const result = Handlebars.compile($animalTemplate);
+  return result(this);
 };
 
-// refactor notes:
-// add arrayname.push(this) to constructor 
-// push animal instance to global array
-// loop through arrays for animal images and keyword filter
-    // render page
-    // set arrays to empty
-    // call functions after empty
 
-AnimalImage.prototype.addAnimalsToPage(filePath) {
-  $.get(filePath, 'json')
+AnimalImage.addAnimalsToPage = (filePath) => {
+  $.get(`data/${filePath}.json`, 'json')
     .then(data => {data.forEach(image => {
       AnimalImage.allImages.push(new AnimalImage(image))
     })})
     .then(renderDropdown)
-    .then(loadImages)
-    .then(loadKeywords)
-  }
-      newAnimal.renderImages(filePath);
-      newAnimal.renderDropdown();
-    });
-  });
+    .then(AnimalImage.loadImages)
+    .then(AnimalImage.loadKeywords)
 }
 
 
@@ -70,58 +42,33 @@ function renderDropdown() {
   })
 }
 
-AnimalImage.prototype.loadImages() {
+AnimalImage.loadImages = () => {
   AnimalImage.allImages.forEach(image => {
-    $('photo-template').append(image.addAnimalsToPage());
+    $('#finalImage').append(image.renderImages());
   })
 }
 
-AnimalImage.prototype.loadKeywords() {
+AnimalImage.loadKeywords = () => {
   AnimalImage.keywords.forEach(keyword => {
     $('dropdown').append(`<option class="filter-remove" value="${keyword}">${keyword}</option>`)
   })
 }
 
-$('#dropdown').on('change', function() {
+$('#filter').on('change', function() {
   let $dropdown = $(this).val();
   $('section').hide();
   $(`section[class="${$dropdown}"]`).show();
 })
 
+$('#dropdown').on('change', function(){
+  $('').remove();
+  $('').remove();
+  let $value = $(this).val();
+  AnimalImage.allImages = [];
+  AnimalImage.keywords = [];
+  AnimalImage.addAnimalsToPage($value);
+})
 
-$('select').on('change', function () {
-  // hide all photos
-  $('.photo').hide();
-  // render photos of animals matching keyword
-  // jQuery .each takes in an index and then a value
-  $('.photo').each((index, photo) => {
-    if ($(this).val() === $(photo).data('keyword')) {
-      $(photo).show();
-    }
-  });
-});
 
-// render data from page-1.json on initial page load
-addAnimalsToPage('data/page-1.json');
 
-// render data from page-2.json on button click
-$('#pagination').on('click', function () {
-  if ($('#pagination').hasClass('page1')) {
-    $('#pagination').text('Previous Page');
-    $('.photo').remove();
-    $('option').not(':first').remove();
-    AnimalImage.filterArray = [];
-    addAnimalsToPage('data/page-2.json');
-    $('.page-2').show();
-    $('#pagination').removeClass('page1').addClass('page2');
-  } else if ($('#pagination').hasClass('page2')) {
-    $('#pagination').text('Next Page');
-    $('.photo').remove();
-    $('option').not(':first').remove();
-    AnimalImage.filterArray = [];
-    addAnimalsToPage('data/page-1.json');
-    $('.page-1').show();
-    $('#pagination').removeClass('page2').addClass('page1');
-  }
-});
 
