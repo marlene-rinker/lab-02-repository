c00382d2fb953e184714f70d57dd561df386b20b
marlene-rinker <marlene.rinker@prtechnologies.com>
2020-05-09 18:45:34 -0700

    fixed page issue with duplicates on first load

--- js/app.js
+++ js/app.js
@@ -1,4 +1,5 @@
 'use strict';
+/* global Handlebars $ */
 
 // TODO: figure out why page-1 is rendering twice
 
@@ -12,18 +13,23 @@ function AnimalImage(image_url, title, description, keyword, horns, page){
   AnimalImage.all.push(this);
 }
 
+AnimalImage.filterArray = [];
+
 // come back to this
-// AnimalImage.prototype.renderFilters = function () {
-//   if(!AnimalImage.filterArray.includes(this.keyword)){
-//     const $animalFilterClone = $('option:first-child').clone();
-//     $animalFilterClone.attr('value', this.keyword);
-//     $animalFilterClone.text(this.keyword);
-//     $('select').append($animalFilterClone);
-//     AnimalImage.filterArray.push(this.keyword);
-//   }
-// };
+AnimalImage.prototype.renderFilters = function () {
+  if(!AnimalImage.filterArray.includes(this.keyword)){
+    const $animalFilterClone = $('option:first-child').clone();
+    $animalFilterClone.attr('value', this.keyword);
+    $animalFilterClone.text(this.keyword);
+    $animalFilterClone.attr('class', this.page);
+    $('select').append($animalFilterClone);
+    AnimalImage.filterArray.push(this.keyword);
+    AnimalImage.filterArray.push(this.page);
+  }
+};
 
 AnimalImage.all = [];
+
 
 AnimalImage.shownImages = 'page-1';
 
@@ -35,13 +41,13 @@ AnimalImage.prototype.renderImages = function (){
 
 function handleData(dataFromFile, pageNumber) {
   dataFromFile.forEach(val => createAnimalImages(val, pageNumber));
-
-  AnimalImage.all.forEach(img => img.renderImages());
 }
 
 function createAnimalImages(animal, page) {
   new AnimalImage(animal.image_url, animal.title, animal.description, animal.keyword, animal.horns, page);
 }
+
+
 
 // switch pages
 const switchPages = () => {
@@ -49,64 +55,67 @@ const switchPages = () => {
   if(AnimalImage.shownImages === 'page-1') {
     AnimalImage.shownImages = 'page-2';
     $('.page-2').show();
-  sortByTitle();
+    sortByTitle();
   } else {
-    AnimalImage.shownImages = 'page-2';
+    AnimalImage.shownImages = 'page-1';
     $('.page-1').show();
-  sortByTitle();
+    sortByTitle();
   }
 }
+
 
 $('#switch').on('click', switchPages);
 
 $.get('data/page-1.json', data => {
   handleData(data, 'page-1');
-  sortByTitle();
 });
 $.get('data/page-2.json', data => {
   handleData(data, 'page-2');
+  sortByTitle();
   $('.page-2').hide();
 })
 
+
 $('#sort-horns').on('click', () => {
   $('main').empty();
-    AnimalImage.all.sort((left, right) => {
-      if(left.horns > right.horns) {
-        return 1;
-      } else if (left.horns < right.horns) {
-        return -1;
-      } else {
-        return 0;
-      }
-    })
-    AnimalImage.all.forEach(img => img.renderImages());
-    $('section').hide();
-    $(`.${AnimalImage.shownImages}`).show();
-  });
+  AnimalImage.all.sort((left, right) => {
+    if(left.horns > right.horns) {
+      return 1;
+    } else if (left.horns < right.horns) {
+      return -1;
+    } else {
+      return 0;
+    }
+  })
+  AnimalImage.all.forEach(img => img.renderImages());
+  $('section').hide();
+  $(`.${AnimalImage.shownImages}`).show();
+});
 
-  $('#sort-title').on('click', () => {
-    sortByTitle();
-  });
+$('#sort-title').on('click', () => {
+  sortByTitle();
+});
+
+// $('#dropdown').on('change', () => {
+//   $('section').hide();
+//   $(`.${AnimalImage.shownImages}`).show();
+
+// });
+
+function sortByTitle() {
+  $('main').empty();
+  AnimalImage.all.sort((left, right) => {
+    if(left.title > right.title) {
+      return 1;
+    } else if (left.title < right.title) {
+      return -1;
+    } else {
+      return 0;
+    }
+  })
+  AnimalImage.all.forEach(img => img.renderImages());
+  $('section').hide();
+  $(`.${AnimalImage.shownImages}`).show();
+}
 
 
-  function sortByTitle() {  
-    $('main').empty();
-      AnimalImage.all.sort((left, right) => {
-        if(left.title > right.title) {
-          return 1;
-        } else if (left.title < right.title) {
-          return -1;
-        } else {
-          return 0;
-        }
-      })
-      AnimalImage.all.forEach(img => img.renderImages());
-      $('section').hide();
-      $(`.${AnimalImage.shownImages}`).show();
-    };
-
-    
-  
-  // select class
-  // if class === horns; change class to title
-  // if class === title; change class to horns
